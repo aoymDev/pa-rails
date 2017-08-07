a58a0c3186f8df82ddb8e95f9f6e8e662e7e76a4
Akiko oyama <akiko.oyama@pasonatech.jp>
2017-08-07 08:31:49 +0000

    チェックボックスform作成

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -3,12 +3,12 @@ class HeloController < ApplicationController
   def index
     if request.post? then
       @title = 'Result'
-      @msg = 'you typed: ' + params['input1'] + '.'
-      @value = params['input1']
+      if params['check1'] then
+        @msg = 'you Checked!'
+      end
     else
       @title = 'Index'
-      @msg = 'type text...'
-      @value = ''
+      @msg = 'check it...'
     end
   end
 
