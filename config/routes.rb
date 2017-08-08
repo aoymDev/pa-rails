ada09b9fdbe8810b0c912551d8f5ea6e6d00edb4
Akiko oyama <akiko.oyama@pasonatech.jp>
2017-08-08 04:35:39 +0000

    伝言板フォーム表示作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
+  get 'dengonban/index'
+  post 'dengonban/index'
+
   get 'helo/index'
   get 'helo', to:'helo#index'
 
