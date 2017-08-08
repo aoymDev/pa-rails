15a86cf2a459148861ae33e57253b91d1462f50e
Akiko oyama <akiko.oyama@pasonatech.jp>
2017-08-08 02:15:54 +0000

    オリジナルレイアウト作成

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,20 +1,10 @@
 class HeloController < ApplicationController
 
   def index
-    if request.post? then
-      @title = 'Result'
-      if params['s1'] then
-        @msg = 'you selected: '
-        for val in params['s1']
-          @msg += val + ' '
-        end
-      else
-        @msg = 'not selected...'
-      end
-    else
-      @title = 'Index'
-      @msg = 'select radio button...'
-    end
+    @header = 'layout sample'
+    @footer = 'copyright AAAAAA 2017'
+    @title = 'New Layout'
+    @msg = 'this is sample page!'
   end
 
   def other
