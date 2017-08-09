86b797611893a3cf446bcf4d75bc75faa3fe159d
Akiko oyama <akiko.oyama@pasonatech.jp>
2017-08-09 00:48:48 +0000

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'people/index'
+  get 'people', to: 'people#index'
+
   get 'dengonban', to: 'dengonban#index'
   post 'dengonban', to: 'dengonban#index'
   get 'dengonban/index'
