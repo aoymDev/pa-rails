f73e61ef6b40b6f0220740611f7aa6454f5adc3c
Akiko oyama <akiko.oyama@pasonatech.jp>
2017-08-09 05:56:42 +0000

    読書カードCRUD作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'cards/index'
+  get 'cards', to: 'cards#index'
+  
+  get 'cards/add'
+  post 'cards/add'
+  
+  get 'cards/:id',  to: 'cards#show'
+  
+  get 'cards/edit/:id', to: 'cards#edit'
+  patch 'cards/edit/:id', to: 'cards#edit'
+  
+  get 'cards/delete/:id', to: 'cards#delete'
+
   get 'people/index'
   get 'people', to: 'people#index'
   get 'people/add'
