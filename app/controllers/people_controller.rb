730fb9f7ca0176e0726ebc5df686d6ecdf708bd9
Akiko oyama <akiko.oyama@pasonatech.jp>
2017-08-09 01:35:51 +0000

    詳細、INSERT処理作成

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,4 +3,27 @@ class PeopleController < ApplicationController
     @msg = 'Person data.'
     @data = Person.all
   end
+
+  def show
+    @msg = "Indexed data."
+    @data = Person.find(params[:id])
+  end
+
+  def add
+    @msg = "add new data."
+  end
+
+  protect_from_forgery
+
+  def create
+    if request.post? then
+      obj = Person.create(
+        name: params['name'],
+        age: params['age'],
+        mail: params['mail']
+      )
+    end
+
+    redirect_to '/people'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'people/index'
   get 'people', to: 'people#index'
+  get 'people/add'
+  post 'people/add', to: 'people#create'
+  get 'people/:id', to: 'people#show'
 
   get 'dengonban', to: 'dengonban#index'
   post 'dengonban', to: 'dengonban#index'
