f890cfca12fdd4dffbf2c22ec8ba5cd540dade1a
Akiko oyama <akiko.oyama@pasonatech.jp>
2017-08-09 03:51:28 +0000

    person更新処理作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'people', to: 'people#index'
   get 'people/add'
   post 'people/add', to: 'people#create'
+  get 'people/edit/:id', to: 'people#edit'
+  patch 'people/edit/:id', to: 'people#update'
   get 'people/:id', to: 'people#show'
 
   get 'dengonban', to: 'dengonban#index'
